6660e590775964d5dfe1c93b14c5c3626d7018d4
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-04-04 18:47:29 -0300

    chore: add tailwind config files

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}"
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
