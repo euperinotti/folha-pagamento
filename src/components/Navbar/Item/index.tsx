e0fa3df54daf33361f1a134eea9c248243727b77
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-04-06 11:11:27 -0300

    refactor: remove unused components

--- src/components/Navbar/Item/index.tsx
+++
@@ -1,7 +0,0 @@
-interface NavbarItemProps {
-  title: string
-}
-
-export const NavbarItem = ({ title }: NavbarItemProps) => {
-  return <div className="py-2 px-4 border-b-2 border-green-500">{title}</div>
-}
