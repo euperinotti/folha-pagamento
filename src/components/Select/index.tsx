41310f6737164aa0a66777016c4792f086ddffd6
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-04-06 11:10:41 -0300

    feat: add select component

---
+++ src/components/Select/index.tsx
@@ -0,0 +1,35 @@
+import { SelectHTMLAttributes } from 'react'
+
+interface SelectProps extends SelectHTMLAttributes<HTMLSelectElement> {
+  label: string
+  options: any[]
+  placeholder: string
+}
+
+export const Select = ({
+  label,
+  options,
+  placeholder,
+  ...rest
+}: SelectProps) => {
+  return (
+    <label className="flex flex-col items-start justify-center gap-2 w-full">
+      <h3>{label}</h3>
+      <select
+        className="w-full bg-slate-200 appearance-none text-lg border-1 border-slate-100 px-2 py-2"
+        {...rest}
+      >
+        <option value={''} disabled>
+          {placeholder}
+        </option>
+        {options.map((option, key) => {
+          return (
+            <option key={key} value={option.value}>
+              {option.name}
+            </option>
+          )
+        })}
+      </select>
+    </label>
+  )
+}
