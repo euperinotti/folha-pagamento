c179d287634a108ea9eed4732dd0603c519022e5
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-04-06 11:12:28 -0300

    style: base component styles

--- src/templates/Base/index.tsx
+++ src/templates/Base/index.tsx
@@ -6,7 +6,7 @@ type BaseProps = {
 
 export const Base = ({ children }: BaseProps) => {
   return (
-    <div className="w-full flex flex-col items-center content-center">
+    <div className="w-full h-dvh flex items-start content-center bg-slate-100 px-8 py-8 gap-10">
       {children}
     </div>
   )
