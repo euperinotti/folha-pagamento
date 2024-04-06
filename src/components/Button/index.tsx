297426ddf62efff4333b44e8b70f68f10c176db9
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-04-06 11:12:08 -0300

    style: button component styles

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -4,7 +4,7 @@ interface ButtonProps {
 
 export const Button = ({ title }: ButtonProps) => {
   return (
-    <button className="px-5 py-2 rounded bg-green-500 text-slate-50">
+    <button className="px-5 py-2 rounded bg-green-600 text-slate-50">
       {title}
     </button>
   )
