df13bd4cbe84ec4184e08ca7536e2aa8cec4eb43
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-04-07 01:11:19 -0300

    styles: input component styles

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -10,7 +10,7 @@ export const Input = ({ label, ...rest }: InputProps) => {
     <InputContainer label={label}>
       <input
         {...rest}
-        className="px-3 py-2 focus:border-green-500 focus:outline-none focus:ring-2 focus:ring-green-500 rounded-sm w-full"
+        className="px-3 py-2 bg-slate-100 focus:border-green-500 focus:outline-none focus:ring-2 focus:ring-green-500 rounded-sm w-full"
       />
     </InputContainer>
   )
