5a1de5c3fae90e576b44a28f4823378e6bf615c2
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-04-07 13:57:29 -0300

    feat: add mocks

---
+++ src/mocks/EmpregadoMock.ts
@@ -0,0 +1,14 @@
+export const _empregadoMock = [
+  {
+    id: '1',
+    nome: 'Empregado 1'
+  },
+  {
+    id: '2',
+    nome: 'Empregado 2'
+  },
+  {
+    id: '3',
+    nome: 'Empregado 3'
+  }
+]
---
+++ src/mocks/EmpresaMock.ts
@@ -0,0 +1,56 @@
+export const _empresaMock = [
+  {
+    id: '1',
+    nome: 'Empresa 1',
+    empregados: [
+      {
+        id: '1',
+        nome: 'Empregado 1'
+      },
+      {
+        id: '2',
+        nome: 'Empregado 2'
+      },
+      {
+        id: '3',
+        nome: 'Empregado 3'
+      }
+    ]
+  },
+  {
+    id: '2',
+    nome: 'Empresa 2',
+    empregados: [
+      {
+        id: '4',
+        nome: 'Empregado 4'
+      },
+      {
+        id: '5',
+        nome: 'Empregado 5'
+      },
+      {
+        id: '6',
+        nome: 'Empregado 6'
+      }
+    ]
+  },
+  {
+    id: '3',
+    nome: 'Empresa 3',
+    empregados: [
+      {
+        id: '7',
+        nome: 'Empregado 7'
+      },
+      {
+        id: '8',
+        nome: 'Empregado 8'
+      },
+      {
+        id: '9',
+        nome: 'Empregado 9'
+      }
+    ]
+  }
+]
