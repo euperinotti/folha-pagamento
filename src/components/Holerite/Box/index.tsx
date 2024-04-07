dbf721aa97dcae26a66d3cd3c27c3f00c10191f6
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-04-07 02:43:18 -0300

    feat: add box component for holerite

---
+++ src/components/Holerite/Box/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface BoxProps {
+  title: string
+  value: string
+}
+
+export const Box = ({ title, value }: BoxProps) => {
+  return (
+    <div>
+      <h5 className="text-slate-400 text-xs font-bold">{title}</h5>
+      <span>{value}</span>
+    </div>
+  )
+}
