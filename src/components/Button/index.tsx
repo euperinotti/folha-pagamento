0c66022762db1868e86e9a9576c24740c8e1692b
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-04-07 01:11:00 -0300

    styles: button component styles

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -4,7 +4,7 @@ interface ButtonProps {
 
 export const Button = ({ title }: ButtonProps) => {
   return (
-    <button className="px-8 py-2 text-lg rounded bg-green-600 hover:bg-green-700 active:bg-green-800 text-slate-50">
+    <button className="px-8 py-2 text-lg rounded-md bg-green-600 hover:bg-green-700 active:bg-green-800 text-slate-50 w-full">
       {title}
     </button>
   )
